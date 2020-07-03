ac3ce485cf8cf8c4aa22c969725e1e8cfff10ae7
Leonardo Henrique da Silva Paixão <42840902+LeohsPaixao@users.noreply.github.com>
2020-07-03 19:05:43 -0300

    Banco Atualizado

--- banco/poo.sql
+++ banco/poo.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2020 at 04:57 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.2
+-- Tempo de geração: 04-Jul-2020 às 00:03
+-- Versão do servidor: 10.4.13-MariaDB
+-- versão do PHP: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,27 +18,35 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
-CREATE DATABASE poo;
-USE poo;
+-- Banco de dados: `poo`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cliente`
+-- Estrutura da tabela `cliente`
 --
 
 CREATE TABLE `cliente` (
-  `idcliente` int(11) NOT NULL,
-  `local` varchar(45) NOT NULL,
+  `usuario` varchar(40) NOT NULL,
+  `endereco` varchar(45) NOT NULL,
+  `email` varchar(60) NOT NULL,
   `senha` varchar(45) NOT NULL,
-  `cartao` varchar(45) NOT NULL
+  `celular` int(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`usuario`, `endereco`, `email`, `senha`, `celular`) VALUES
+('leonardo', 'Rua São Paulo ', 'leo@email.com', '12345', 12),
+('rafa', 'rafa', 'rafa', 'rafa', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `corrida`
+-- Estrutura da tabela `corrida`
 --
 
 CREATE TABLE `corrida` (
@@ -52,20 +59,31 @@ CREATE TABLE `corrida` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mototaxi`
+-- Estrutura da tabela `mototaxi`
 --
 
 CREATE TABLE `mototaxi` (
-  `idmototaxi` int(11) NOT NULL,
+  `nome` varchar(40) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
+  `usuario` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   `senha` varchar(45) NOT NULL,
-  `placa` varchar(45) NOT NULL,
-  `cnh` varchar(45) NOT NULL
+  `rg` int(15) NOT NULL,
+  `cnh` varchar(45) NOT NULL,
+  `email` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
+  `telefone` int(15) NOT NULL,
+  `celular` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `mototaxi`
+--
+
+INSERT INTO `mototaxi` (`nome`, `usuario`, `senha`, `rg`, `cnh`, `email`, `telefone`, `celular`) VALUES
+('jao', 'jao', 'jao', 0, 'jao', 'jao', 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pagamento`
+-- Estrutura da tabela `pagamento`
 --
 
 CREATE TABLE `pagamento` (
@@ -75,7 +93,7 @@ CREATE TABLE `pagamento` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pessoa`
+-- Estrutura da tabela `pessoa`
 --
 
 CREATE TABLE `pessoa` (
@@ -87,33 +105,40 @@ CREATE TABLE `pessoa` (
   `idpessoa` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Indexes for dumped tables
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idusuario` int(11) NOT NULL,
+  `nomeusuario` varchar(60) NOT NULL,
+  `senha` varchar(60) NOT NULL,
+  `Type` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`idcliente`);
-
---
--- Indexes for table `mototaxi`
---
-ALTER TABLE `mototaxi`
-  ADD PRIMARY KEY (`idmototaxi`);
-
---
--- Indexes for table `pagamento`
+-- Índices para tabela `pagamento`
 --
 ALTER TABLE `pagamento`
   ADD PRIMARY KEY (`idpagamento`);
 
 --
--- Indexes for table `pessoa`
+-- Índices para tabela `pessoa`
 --
 ALTER TABLE `pessoa`
   ADD PRIMARY KEY (`idpessoa`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idusuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
