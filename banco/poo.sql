91cfd7cd00d09ed9d91db8b5e036c08bfe054913
Atreuz <rafaelcampanincarneiro@gmail.com>
2020-07-03 19:50:05 -0300

    Teste de cadastro

--- banco/poo.sql
+++ banco/poo.sql
@@ -1,24 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 04-Jul-2020 às 00:03
--- Versão do servidor: 10.4.13-MariaDB
--- versão do PHP: 7.4.7
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `poo`
+﻿USE poo;
 --
 
 -- --------------------------------------------------------
