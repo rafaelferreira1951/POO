15b94251c4454a149a00561ee48fa026e0c7f6d8
@rafaelferreira1951 <rafaelferreira.1951@gmail.com>
2020-07-07 22:46:43 -0300

    com o banquinho

--- banco/poo.sql
+++ banco/poo.sql
@@ -1,11 +1,31 @@
-﻿
-USE poo;
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 08, 2020 at 03:46 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `poo`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cliente`
+-- Table structure for table `cliente`
 --
 
 CREATE TABLE `cliente` (
@@ -17,33 +37,36 @@ CREATE TABLE `cliente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Extraindo dados da tabela `cliente`
+-- Dumping data for table `cliente`
 --
 
 INSERT INTO `cliente` (`usuario`, `endereco`, `email`, `senha`, `celular`) VALUES
-('leonardo', 'Rua São Paulo ', 'leo@email.com', '12345', 12),
-('rafa', 'rafa', 'rafa', 'rafa', 0);
+('asdf', 'asdf', 'asdf', 'asdf', 0);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `corrida`
+-- Table structure for table `corrida`
 --
 
 CREATE TABLE `corrida` (
-  `idcorrida` int(11) NOT NULL,
-  `distancia` varchar(45) NOT NULL,
-  `tempo` varchar(45) NOT NULL,
-  `valortotal` varchar(45) NOT NULL
+  `id` int(11) NOT NULL,
+  `motoboy` varchar(500) NOT NULL,
+  `cliente` varchar(500) NOT NULL,
+  `destino` varchar(500) NOT NULL,
+  `localizacao` varchar(500) NOT NULL,
+  `pagamento` varchar(500) NOT NULL,
+  `valor` varchar(500) NOT NULL,
+  `status` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `mototaxi`
+-- Table structure for table `motoboy`
 --
 
-CREATE TABLE `mototaxi` (
+CREATE TABLE `motoboy` (
   `nome` varchar(40) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
   `usuario` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   `senha` varchar(45) NOT NULL,
@@ -55,16 +78,16 @@ CREATE TABLE `mototaxi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Extraindo dados da tabela `mototaxi`
+-- Dumping data for table `motoboy`
 --
 
-INSERT INTO `mototaxi` (`nome`, `usuario`, `senha`, `rg`, `cnh`, `email`, `telefone`, `celular`) VALUES
-('jao', 'jao', 'jao', 0, 'jao', 'jao', 0, 0);
+INSERT INTO `motoboy` (`nome`, `usuario`, `senha`, `rg`, `cnh`, `email`, `telefone`, `celular`) VALUES
+('zxcv', 'zxcv', 'zxcv', 0, 'zxcv', 'zxcv', 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `pagamento`
+-- Table structure for table `pagamento`
 --
 
 CREATE TABLE `pagamento` (
@@ -74,7 +97,7 @@ CREATE TABLE `pagamento` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `pessoa`
+-- Table structure for table `pessoa`
 --
 
 CREATE TABLE `pessoa` (
@@ -89,7 +112,7 @@ CREATE TABLE `pessoa` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuarios`
+-- Table structure for table `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -100,24 +123,44 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `pagamento`
+-- Indexes for table `corrida`
+--
+ALTER TABLE `corrida`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pagamento`
 --
 ALTER TABLE `pagamento`
   ADD PRIMARY KEY (`idpagamento`);
 
 --
--- Índices para tabela `pessoa`
+-- Indexes for table `pessoa`
 --
 ALTER TABLE `pessoa`
   ADD PRIMARY KEY (`idpessoa`);
 
 --
--- Índices para tabela `usuarios`
+-- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`idusuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `corrida`
+--
+ALTER TABLE `corrida`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
